7b7907a0eca405eb1a9a9582cc3fb2d89851715f
Jed <jed@m6700.local>
2018-09-19 23:58:51 -0400

    .

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # python_GRBL
 Pythonic wrapper for GRBL control.
+
+
+# Usage & Examples
+
+
+# Installation
+
+Through pip:
+
+    pip install git+https://github.com/jed-frey/python_GRBL.git
+
+or in requirements.txt:
+
+    git+https://github.com/jed-frey/python_GRBL.git
---
+++ setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+import versioneer
+
+import os
+
+LOCAL_DIR = os.path.dirname(os.path.abspath(__file__))
+
+def read_requirements(path="requirements.txt"):
+    """Read requirements file relative to setup.py"""
+    full_path = os.path.join(LOCAL_DIR, path)
+    def yield_line(path):
+        with open(path, "r") as fid:
+            for line in fid.readlines():
+                yield line
+
+    return [requirement.strip() for requirement in yield_line(full_path) if not requirement.startswith("#")]
+
+requirements = read_requirements()
+test_requirements = read_requirements(path="requirements_test.txt")
+
+setuptools.setup(
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    name="GRBL",
+    description="GRBL",
+    author="Jed",
+    license="BSD",
+    packages=setuptools.find_packages(),
+    zip_safe=False,
+    install_requires=requirements,
+    extras_require={"test": test_requirements},
+    include_package_data=True,
+)
