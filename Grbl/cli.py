43a3fb9a0159768e2c71adc83bb2f62aa7ffe709
jed <jed+make_sandwich@e1-LINUX>
2019-10-24 22:37:02 -0400

    .

--- Grbl/cli.py
+++ Grbl/cli.py
@@ -1,24 +1,46 @@
-"""Naval Fate.
+# -*- coding: utf-8 -*-
+"""Module for the ```grbl2.py``` command line interface.
 
-Usage:
-  grbl.py aimlaser [--port=<port>]
-  grbl.py (-h | --help)
-  grbl.py --version
-
-Options:
-  -h --help     Show this screen.
-  --version     Show version.
-  --port=<port>  Speed in knots [default: /dev/ttyUSB0].
-
+An entrypoint for ```grbl2``` module.
 """
-from docopt import docopt
+import os
+
+import click
+
+import Grbl
 
 
+@click.group()
+@click.version_option()
 def cli():
-    arguments = docopt(__doc__, version="Naval Fate 2.0")
-    print(arguments)
+    """Grbl command line interface entry point.
+
+    grbl.py is a utility for interacting with Grbl from the
+    command line.
+    """
 
 
-if __name__ == "__main__":
-    arguments = docopt(__doc__, version="Naval Fate 2.0")
-    print(arguments)
+@cli.command("aimlaser")
+@click.option(
+    "--port",
+    metavar="port",
+    default="/dev/ttyUSB0",
+    help="Weight file. [Default: {}]".format("/dev/ttyUSB0"),
+)
+@click.option(
+    "--baudrate",
+    metavar="baudrate",
+    default=115200,
+    help="Weight file. [Default: 115200]",
+)
+def aimlaser(port: str, baudrate: int):
+    """Aim the laser.
+
+    Turns the laser on to the minimal power setting for aiming.
+    Example
+    -------
+
+    $ grbl2.py aimlaser
+    """
+    grbl = Grbl.Grbl(port=port)
+    grbl.aim_laser()
